ed653434e5c9840d0bfcff765815318ecb303fb9
mirokate <109084515+mirokate@users.noreply.github.com>
2022-07-21 15:34:54 +0300

    Add files via upload

---
+++ 1-1.py
@@ -0,0 +1,11 @@
+import  math
+
+x  =  float ( input ( 'Введіть x: ' ))
+y  =  float ( input ( 'Введіть y: ' ))
+z  =  float ( input ( 'Введіть z: ' ))
+
+a  = 1
+b  = 1
+
+print ( 'Рівняння a= ' , a )
+print ( 'Рівняння b= ' , b )
