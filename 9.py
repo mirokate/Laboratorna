2a5ba3756ab3c002620b6cd45f74e3122aa0f3d5
mirokate <109084515+mirokate@users.noreply.github.com>
2022-07-21 15:34:40 +0300

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,7 @@
+begin
+  Write('"Пташине молоко"   '); Write('1 кг   '); Writeln('80 грн');
+  Write('"Тістеко"  '); Write('250 гр '); Writeln('50 грн');
+  Write('"Салат"  '); Write('125 гр'); Writen('45 грн')
+  Write('"Кола-кола"  '); Write('150 мл'); Writen('20 грн')
+  
+end.
