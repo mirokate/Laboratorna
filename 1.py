1f1ecc20ca04f11eb8a0a8ab0251aa834c5d6bb7
mirokate <109084515+mirokate@users.noreply.github.com>
2022-07-21 15:25:59 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+import math
+
+x = float(input('Введіть x(від 5 до 25): '))
+
+if n = 1:
+    s = a*b
+    print(y)
+elif n = 2:
+    s = a * (h/2)
+    print(s)
+elif n = 3
+    s = ((a+b)*(h/2))
+    print(s)
+elif n = 4:
+    s = (pi * R^2)
+    print(s)
+elif n = 5:
+    s = ((pi * R^2)*(a/360))
+    print(s)
+else:
+    print('Ви ввели не те число!Попробуйте ще раз')
