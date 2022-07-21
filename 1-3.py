23daa7f8fc96203549c409bccb2b2c01f9f59448
mirokate <109084515+mirokate@users.noreply.github.com>
2022-07-21 15:36:21 +0300

    Add files via upload

---
+++ 1-3.py
@@ -0,0 +1,13 @@
+from random import randint
+
+amount = 10
+numbers = []
+
+for i in range(amount):
+    numbers.append(randint(-20, 20))
+
+print(numbers)
+print(numbers[0])
+print(numbers[1], numbers[2])
+print(numbers[3], numbers[4], numbers[5])
+print(numbers[6], numbers[7], numbers[8], numbers[9])
